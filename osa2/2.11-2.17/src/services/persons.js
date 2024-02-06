b8c2510190f6a49a8c33eddc80d6782994273dfc
Riki <riki.jarvinen@gmail.com>
2024-02-06 03:01:32 +0200

    2.11-2.17

---
+++ osa2/2.11-2.17/src/services/persons.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+const baseurl = "http://localhost:3001/persons";
+
+const getAll = () => {
+    return axios.get(baseurl);
+}
+
+const create = (newObject) => {
+    return axios.post(baseurl, newObject);
+}
+
+const update = (id, newObject) => {
+    return axios.put(`${baseurl}/${id}`, newObject);
+}
+
+const remove = (id) => {
+    return axios.delete(`${baseurl}/${id}`);
+}
+
+export default { 
+    getAll: getAll, 
+    create: create, 
+    update: update,
+    remove: remove
+};
